7323fe4770a6db1ac88dd7d3d1f3923b551b3122
xmetal <xmetalfanx@yahoo.com>
2020-03-10 20:51:32 -0400

    Added basic arch framework

--- getVersionData.sh
+++ getVersionData.sh
@@ -1,3 +1,12 @@
+function userPrompt() {
+  read -p "Press [Enter] to continue "
+}
+
 # include file to get Debian Version info 
+echo -e "Getting Arch Linux info"
+. getArchVersions.cfg
+userPrompt
+
+
 echo -e "Getting Debian info"
 . getDebianVersions.cfg
