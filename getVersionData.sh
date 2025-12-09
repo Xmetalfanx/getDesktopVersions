26a36906d4ad2b36c73685513cd7d3bfdcde8a1d
Xmetalfanx <Xmetalfanx@yahoo.com>
2025-12-09 13:44:47 -0500

    Moved arch display call

--- getVersionData.sh
+++ getVersionData.sh
@@ -39,7 +39,6 @@ function display_desktop_info() {
   echo -e "------------------------------"
   echo -e "$desktop INFO"
 
-  display_arch_info
   display_debian_info
   display_fedora_info
   display_opensuse_info
