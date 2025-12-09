5557bc24ad4c14d288dc45a2c3bfea27c66a0340
Xmetalfanx <Xmetalfanx@yahoo.com>
2025-12-09 13:46:02 -0500

    Moved Ubuntu display call

--- getVersionData.sh
+++ getVersionData.sh
@@ -40,7 +40,6 @@ function display_desktop_info() {
   echo -e "$desktop INFO"
 
   display_opensuse_info
-  display_ubuntu_info
 }
 
 ###########################################################################
